4c7d0ae07074a348d8f18f5467536e966391ffe1
Vitrola1302 <vag@cesar.school>
2023-04-16 10:58:22 -0300

    registro e login funcionáis, erro nos redirect's

--- src/streakchat/forms.py
+++ src/streakchat/forms.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- src/streakchat/views.py
+++ src/streakchat/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 def registerPage(request):
-    form = RegisterForm()
-
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
-
-    return render(request, 'accounts/register.html', {'form':form})
+            user = form.save()
+            messages.success(request, 'Registration Successful.')
+            return redirect('loginPage')
+        else:
+            messages.error(request, 'Unsuccessful registration. Invalid information.')
+    else:
+        form = RegisterForm()
+        return render (request=request, template_name='accounts/register.html', context={'form':form})
 
 def loginPage(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}')
+                return redirect('landingPage')
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, "Invalid username or password")
+    form = AuthenticationForm()
+    return render(request, 'accounts/login.html', context={'form':form})
 
 def landingPage(request):
     return render(request, 'landingpage/index.html')
